d5b8477b286cf10993ea6532d0a675c504fa0372
Jaser Akuly <Jaser@theRRD.com>
2015-10-06 01:27:22 -0500

    zip code and country

--- js/controllers.js
+++ js/controllers.js
@@ -8,16 +8,27 @@ weatherApp.controller('weatherCtrl', function ($scope, $http) {
     $scope.hello = 'Hi!';
     $scope.user = {};
 
-    $scope.checkWeather = function(loc,format) {
+    $scope.checkWeather = function (loc, zip, country, format) {
         $scope.location_entered = loc;
 
         var url = 'http://api.openweathermap.org/data/2.5/weather?&APPID=7c99bd12f8e57f1557fe0472e9ea0a64&q=' + loc;
 
-                    if (format === 'Celsius') {
-                        url += "&units=metric";
-                    } else if (format === 'Fahrenheit') {
-                        url += "&units=imperial";
-                    }
+        //change how they generate the url
+        if (loc !== undefined) {
+            url += '&q=' + loc;
+        } else if (zip !== undefined && country !== undefined) {
+            url += 'zip=' + zip + ',' + country;
+        } else {
+            alert("Error, please provide either city or zip & country!");
+            return;
+        }
+
+        //checking what the format the user provided ask the api the corresponding weather
+        if (format === 'Celsius') {
+            url += "&units=metric";
+        } else if (format === 'Fahrenheit') {
+            url += "&units=imperial";
+        }
 
 
         //retrieve weather data from the website adding + location the user provided above
