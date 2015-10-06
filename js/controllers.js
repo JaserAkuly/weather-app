27b236c526ed84e9f9cd0cae32c70b68f0a32fe4
Jaser Akuly <Jaser@theRRD.com>
2015-10-06 00:44:04 -0500

    Ask user for location

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,13 @@
 /**
  * Created by JaserAkuly on 10/5/15.
  */
+
 var weatherApp = angular.module('weatherApp', []);
 
 weatherApp.controller('weatherCtrl', function ($scope) {
-    $scope.hello = 'Hi! Testing if the scope variable works!';
+    $scope.hello = 'Hi!';
+
+    $scope.checkWeather = function(loc) {
+        $scope.location_entered = loc;
+    };
 });
