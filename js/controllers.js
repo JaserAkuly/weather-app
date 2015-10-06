8230790ab926c29a34a108a2813062d28cdf8ce9
Jaser Akuly <Jaser@theRRD.com>
2015-10-06 01:04:39 -0500

    Display Weather

--- js/controllers.js
+++ js/controllers.js
@@ -8,15 +8,25 @@ weatherApp.controller('weatherCtrl', function ($scope, $http) {
     $scope.hello = 'Hi!';
     $scope.user = {};
 
-    $scope.checkWeather = function(loc) {
+    $scope.checkWeather = function(loc,format) {
         $scope.location_entered = loc;
+
+        var url = 'http://api.openweathermap.org/data/2.5/weather?&APPID=7c99bd12f8e57f1557fe0472e9ea0a64&q=' + loc;
+
+                    if (format === 'Celsius') {
+                        url += "&units=metric";
+                    } else if (format === 'Fahrenheit') {
+                        url += "&units=imperial";
+                    }
+
 
         //retrieve weather data from the website adding + location the user provided above
         $http({
             method: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/weather?&APPID=7c99bd12f8e57f1557fe0472e9ea0a64&q=' + loc
+            url: url
         }).then(function successCallback(response) {
             console.log("Got success!", response);
+            $scope.temperature = response.data.main.temp;
         }, function errorCallback(response) {
             console.log("Got error!", response);
         });
